7823089caf2884090ea09045c092ff2be3b94192
lianguo <angoo_lee@hotmail.com>
2024-04-28 11:42:50 +0800

    Update meta_merge.py

--- meta_merge.py
+++ meta_merge.py
@@ -270,8 +270,8 @@ with open('./templates/clash_template.yaml', 'r', encoding='utf-8') as file:
     config_data = yaml.safe_load(file)
 
 # 读取warp配置文件内容
-with open('./templates/clash_warp_template.yaml', 'r', encoding='utf-8') as file:
-    config_warp_data = yaml.safe_load(file)
+# with open('./templates/clash_warp_template.yaml', 'r', encoding='utf-8') as file:
+#    config_warp_data = yaml.safe_load(file)
 
 # 添加合并后的代理到proxies部分
 if 'proxies' not in config_data or not config_data['proxies']:
@@ -279,22 +279,22 @@ if 'proxies' not in config_data or not config_data['proxies']:
 else:
     config_data['proxies'].extend(merged_proxies)
 
-if 'proxies' not in config_warp_data or not config_warp_data['proxies']:
-    config_warp_data['proxies'] = merged_proxies
-else:
-    config_warp_data['proxies'].extend(merged_proxies)
+# if 'proxies' not in config_warp_data or not config_warp_data['proxies']:
+#     config_warp_data['proxies'] = merged_proxies
+# else:
+#     config_warp_data['proxies'].extend(merged_proxies)
 
 
 # 更新自动选择和节点选择的proxies的name部分
 update_proxy_groups(config_data, merged_proxies)
-update_warp_proxy_groups(config_warp_data, merged_proxies)
+# update_warp_proxy_groups(config_warp_data, merged_proxies)
 
 # 将更新后的数据写入到一个YAML文件中，并指定编码格式为UTF-8
 with open('./sub/merged_proxies_new.yaml', 'w', encoding='utf-8') as file:
     yaml.dump(config_data, file, sort_keys=False, allow_unicode=True)
 
-with open('./sub/merged_warp_proxies_new.yaml', 'w', encoding='utf-8') as file:
-    yaml.dump(config_warp_data, file, sort_keys=False, allow_unicode=True)
+# with open('./sub/merged_warp_proxies_new.yaml', 'w', encoding='utf-8') as file:
+#     yaml.dump(config_warp_data, file, sort_keys=False, allow_unicode=True)
 
 print("聚合完成")
 
